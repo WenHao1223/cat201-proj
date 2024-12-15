a96ef0f5f9ff045c2c4dc0c883fdc3a34784c95b
WenHao1223 <wenhaojshs@gmail.com>
2024-12-16 01:22:48 +0800

    Setup test cases

---
+++ src/test/java/com/utils/ReadJsonTest.java
@@ -0,0 +1,24 @@
+package com.utils;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.chefsAura.utils.ReadJson;
+import org.json.JSONArray;
+import org.junit.jupiter.api.Test;
+
+public class ReadJsonTest {
+
+    @Test
+    public void testReadUserJson() {
+        ReadJson reader = new ReadJson();
+        JSONArray userData = reader.readJson("user");
+        assertEquals("jdoe123", userData.getJSONObject(0).getString("username"));
+    }
+
+    @Test
+    public void testReadProductJson() {
+        ReadJson reader = new ReadJson();
+        JSONArray productData = reader.readJson("product");
+        assertEquals("Chef Knife", productData.getJSONObject(0).getString("name"));
+    }
+}
