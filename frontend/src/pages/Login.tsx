b94ae8bdc5b8cdbd54b1f4cd31049568980d09fe
WenHao1223 <wenhaojshs@gmail.com>
2025-01-02 02:43:39 +0800

    Improve UI

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+
 import "@styles/LoginRegister.css";
 
 const Login: React.FC = () => {
@@ -9,7 +10,9 @@ const Login: React.FC = () => {
         setEmail(event.target.value);
     };
 
-    const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const handlePasswordChange = (
+        event: React.ChangeEvent<HTMLInputElement>
+    ) => {
         setPassword(event.target.value);
     };
 
@@ -27,19 +30,26 @@ const Login: React.FC = () => {
                     <i className="fas fa-kitchen-set"></i> Chef's Aura
                 </div>
                 <div className="form-container">
-                    <div className="image-container" style={{ marginTop: "20px" }}>
+                    <div
+                        className="image-container"
+                        style={{ marginTop: "20px" }}
+                    >
                         <img
                             src="https://th.bing.com/th/id/OIP.RwtkstiEHa-_o2QIKYBy3QHaE8?w=238&h=188&c=7&r=0&o=5&dpr=1.1&pid=1.7"
                             alt="Sample photo"
                             className="img-fluid"
-                            style={{ width: "400px", height: "200px", borderRadius: "15px" }}
+                            style={{
+                                width: "400px",
+                                height: "200px",
+                                borderRadius: "15px",
+                            }}
                         />
                     </div>
                     <div className="form-content">
                         <header style={{ textAlign: "center" }}>Login</header>
                         <form onSubmit={handleSubmit} className="form">
                             <div className="input-box">
-                                <i className="fas fa-envelope"></i>
+                                <i className="fas fa-envelope"></i> Email
                                 <input
                                     type="email"
                                     placeholder="Enter your email"
@@ -49,7 +59,7 @@ const Login: React.FC = () => {
                                 />
                             </div>
                             <div className="input-box">
-                                <i className="fas fa-lock"></i>
+                                <i className="fas fa-lock"></i> Password
                                 <input
                                     type="password"
                                     placeholder="Enter your password"
@@ -59,9 +69,17 @@ const Login: React.FC = () => {
                                 />
                             </div>
                             <div className="button-container">
-                                <button type="submit" className="btn btn-full-width btn-warning ms-2">Submit form</button>
+                                <button
+                                    type="submit"
+                                    className="btn btn-full-width btn-warning ms-2"
+                                >
+                                    Submit form
+                                </button>
                             </div>
-                            <div className="text sign-up-text">Don't have an account? <a href="/register">Signup now</a></div>
+                            <div className="text sign-up-text pt-4">
+                                Don't have an account?{" "}
+                                <a href="/register">Sign up now</a>
+                            </div>
                         </form>
                     </div>
                 </div>
