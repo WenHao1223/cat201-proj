c434301ee0fc2f678e33dfaae3316cd36ef7c089
WenHao1223 <wenhaojshs@gmail.com>
2025-01-12 05:35:10 +0800

    Show user

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -787,6 +787,28 @@ const Profile: React.FC<ProfileProps> = ({
                                         </span>
                                     )}
                                 </div>
+                                <div className="profile-item">
+                                    <label className="block text-sm font-medium text-gray-700">
+                                        Role:
+                                    </label>
+                                    {isEditing ? (
+                                        <input
+                                            type="text"
+                                            value={editableUserDetails?.role}
+                                            onChange={(e) =>
+                                                handleInputChange(
+                                                    "role",
+                                                    e.target.value
+                                                )
+                                            }
+                                            className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                                        />
+                                    ) : (
+                                        <span className="block font-light">
+                                            {currentUserGeneralDetails?.role}
+                                        </span>
+                                    )}
+                                </div>
                             </div>
                             <button
                                 onClick={isEditing ? saveProfile : editProfile}
